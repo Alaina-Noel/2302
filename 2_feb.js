93ee577b19581b734093ef5263b55147d958cef1
Alaina Kneiling <alaina.noel.8@gmail.com>
2023-02-02 17:36:45 -0700

    more promises

--- 2_feb.js
+++ 2_feb.js
@@ -1,16 +1,37 @@
-myPromise = new Promise(resolve => 
+let myPromise = new Promise(resolve => 
   resolve('contents of the promise')
   );
 
-myShorterPromise = Promise.resolve('contents of the promise');
+let myShorterPromise = Promise.resolve('contents of the promise');
 
 //the good thing about the top syntax is you can do things like this
 
-myRejectedPromise = new Promise((resolve, reject) => {
-  const apiCallStatusCode = 201;
-    if (apiCallStatusCode === 201) {
-      resolve('happy dance');
-    } else {
-      reject('contents');
-    }
+let myRejectedPromise = new Promise((resolve, reject) => {
+const apiCallStatusCode = 201;
+  if (apiCallStatusCode === 201) {
+    resolve('happy dance');
+  } else {
+    reject('contents');
+  }
+});
+
+const array = [];
+array.push('Meg');
+
+const turingPromise = Promise.resolve('Chris')
+  .then(() => {
+    array.push('Brian');
+  })
+  .then(() => {
+    array.push('Abdul');
+  })
+  .then(() => {
+    return array;
   });
+
+array.push('Erin');
+array;
+turingPromise;
+
+
+
